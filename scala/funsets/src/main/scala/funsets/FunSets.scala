a9ee72eb348b9c70e9bef826a853ad808767eb57
Wojciech Gawroński <afronski@gmail.com>
2012-09-25 21:58:04 +0200

    [Scala] Week 2 - Assignment finished

--- scala/funsets/src/main/scala/funsets/FunSets.scala
+++ scala/funsets/src/main/scala/funsets/FunSets.scala
@@ -20,30 +20,30 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): Set = ???
+  def singletonSet(elem: Int): Set = (x) => x == elem
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = ???
+  def union(s: Set, t: Set): Set = (x : Int) => contains(s, x) || contains(t, x)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` or `t`.
    */
-  def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set = (x : Int) => contains(s, x) && contains(t, x)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = ???
+  def diff(s: Set, t: Set): Set = (x: Int) => if (contains(s, x)) !contains(t, x) else false
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = ???
+  def filter(s: Set, p: Int => Boolean): Set = intersect(s, p)
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -55,23 +55,33 @@ object FunSets {
    */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (contains(s, a) && !p(a)) false 
+      else iter(a + 1)
     }
-    iter(???)
+    
+    iter(-bound)
   }
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    def iter(a: Int): Boolean = {
+      if (a > bound) false
+      else if (contains(s, a) && p(a)) true 
+      else iter(a + 1)
+    }
+    
+    iter(-bound)
+  }
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set =
+    (x: Int) => contains(s, f(x))
 
   /**
    * Displays the contents of a set
--- scala/funsets/src/test/scala/funsets/FunSetSuite.scala
+++ scala/funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -5,108 +5,181 @@ import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-/**
- * This class is a test suite for the methods in object FunSets. To run
- * the test suite, you can either:
- *  - run the "test" command in the SBT console
- *  - right-click the file in eclipse and chose "Run As" - "JUnit Test"
- */
 @RunWith(classOf[JUnitRunner])
 class FunSetSuite extends FunSuite {
-
-
-  /**
-   * Link to the scaladoc - very clear and detailed tutorial of FunSuite
-   *
-   * http://doc.scalatest.org/1.8/index.html#org.scalatest.FunSuite
-   *
-   * Operators
-   *  - test
-   *  - ignore
-   *  - pending
-   */
-
-  /**
-   * Tests are written using the "test" operator and the "assert" method.
-   */
-  test("string take") {
-    val message = "hello, world"
-    assert(message.take(5) == "hello")
-  }
-
-  /**
-   * For ScalaTest tests, there exists a special equality operator "===" that
-   * can be used inside "assert". If the assertion fails, the two values will
-   * be printed in the error message. Otherwise, when using "==", the test
-   * error message will only say "assertion failed", without showing the values.
-   *
-   * Try it out! Change the values so that the assertion fails, and look at the
-   * error message.
-   */
-  test("adding ints") {
-    assert(1 + 2 === 3)
-  }
-
   
   import FunSets._
 
   test("contains is implemented") {
     assert(contains(x => true, 100))
   }
-  
-  /**
-   * When writing tests, one would often like to re-use certain values for multiple
-   * tests. For instance, we would like to create an Int-set and have multiple test
-   * about it.
-   * 
-   * Instead of copy-pasting the code for creating the set into every test, we can
-   * store it in the test class using a val:
-   * 
-   *   val s1 = singletonSet(1)
-   * 
-   * However, what happens if the method "singletonSet" has a bug and crashes? Then
-   * the test methods are not even executed, because creating an instance of the
-   * test class fails!
-   * 
-   * Therefore, we put the shared values into a separate trait (traits are like
-   * abstract classes), and create an instance inside each test method.
-   * 
-   */
 
   trait TestSets {
-    val s1 = singletonSet(1)
-    val s2 = singletonSet(2)
-    val s3 = singletonSet(3)
+    val firstSet = singletonSet(1)
+    val secondSet = singletonSet(2)
+    val thirdSet = singletonSet(3)
   }
 
-  /**
-   * This test is currently disabled (by using "ignore") because the method
-   * "singletonSet" is not yet implemented and the test would fail.
-   * 
-   * Once you finish your implementation of "singletonSet", exchange the
-   * function "ignore" by "test".
-   */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSets tests") {
     
-    /**
-     * We create a new instance of the "TestSets" trait, this gives us access
-     * to the values "s1" to "s3". 
-     */
     new TestSets {
-      /**
-       * The string argument of "assert" is a message that is printed in case
-       * the test fails. This helps identifying which assertion failed.
-       */
-      assert(contains(s1, 1), "Singleton")
+      assert(contains(firstSet, 1), "Singleton s1")
+      assert(contains(secondSet, 2), "Singleton s2")
+      assert(contains(thirdSet, 3), "Singleton s3")
+      
+      assert(!contains(firstSet, 2), "Singleton s1 => false case")
+      assert(!contains(secondSet, 3), "Singleton s2 => false case")
+      assert(!contains(thirdSet, 1), "Singleton s3 => false case")
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
-      val s = union(s1, s2)
-      assert(contains(s, 1), "Union 1")
-      assert(contains(s, 2), "Union 2")
-      assert(!contains(s, 3), "Union 3")
+      val result = union(firstSet, secondSet)
+      
+      assert(contains(result, 1), "Union 1")
+      assert(contains(result, 2), "Union 2")
+      assert(!contains(result, 3), "Union 3")
     }
   }
+  
+  test("union of custom sets") {
+    val firstSet = (x : Int) => x > 10
+    val secondSet = (x : Int) => x < 20
+    val result = union(firstSet, secondSet)
+    
+    assert(contains(result, 1))
+    assert(contains(result, 10))
+    assert(contains(result, 20))
+    assert(contains(result, 100))    
+    assert(contains(result, 9))
+    assert(contains(result, 19))
+    assert(contains(result, 15))
+  }
+  
+  test("insersect two excluding sets") {
+    new TestSets {
+      val result = intersect(firstSet, secondSet)
+      
+      assert(!contains(result, 1), "Intersect 1")
+      assert(!contains(result, 2), "Intersect 2")
+      assert(!contains(result, 3), "Intersect 3")
+    }
+  }
+  
+  test("intersect of custom sets") {
+    val firstSet = (x : Int) => x > 10
+    val secondSet = (x : Int) => x < 20
+    val result = intersect(firstSet, secondSet)
+    
+    assert(!contains(result, 1), "Set cannot contains 1")
+    assert(!contains(result, 10), "Set cannot contains 10")
+    assert(!contains(result, 20), "Set cannot contains 20")
+    assert(!contains(result, 100), "Set cannot contains 100")
+    
+    assert(contains(result, 11), "Set must contains 11")
+    assert(contains(result, 19), "Set must contains 19")
+    assert(contains(result, 15), "Set must contains 15")
+  }  
+  
+  test("diff of custom sets") {
+    val firstSet = (x : Int) => x > 10
+    val secondSet = (x : Int) => x > 20
+    val result = diff(firstSet, secondSet)
+    
+    assert(!contains(result, 1), "Set mustn't contains 1")
+    assert(!contains(result, 10), "Set must'nt contains 10")   
+    assert(contains(result, 11), "Set must contains 11")
+    assert(contains(result, 19), "Set must contains 19")
+    assert(contains(result, 15), "Set must contains 15")
+    assert(!contains(result, 21), "Set mustn't contains 21")
+    assert(!contains(result, 100), "Set mustn't contains 100")
+  }    
+  
+  test("filter is an intersection") {
+    val set = (x : Int) => x > 10
+    val result = filter(set, (x: Int) => x < 20)
+    
+    assert(!contains(result, 1), "Set cannot contains 1")
+    assert(!contains(result, 10), "Set cannot contains 10")
+    assert(!contains(result, 20), "Set cannot contains 20")
+    assert(!contains(result, 100), "Set cannot contains 100")
+    
+    assert(contains(result, 11), "Set must contains 11")
+    assert(contains(result, 19), "Set must contains 19")
+    assert(contains(result, 15), "Set must contains 15")
+  }   
+  
+ test("forall test") {
+    val set = (x : Int) => x > 10
+    
+    val predicate1 = (x : Int) => x > 10
+    val predicate2 = (x : Int) => x > 11
+    val predicate3 = (x : Int) => x < -9
+    val predicate4 = (x : Int) => x != 0
+    
+    val predicate5 = (x : Int) => x > 10000
+    val predicate6 = (x : Int) => x < -10000
+    
+    assert(forall(set, predicate1), "Test 1")
+    assert(!forall(set, predicate2), "Test 2")
+    assert(!forall(set, predicate3), "Test 3")
+    assert(forall(set, predicate4), "Test 4")
+    
+    assert(!forall(set, predicate5), "Test 5")
+    assert(!forall(set, predicate6), "Test 6")
+  }    
+ 
+ test("exists test") {
+    val set = (x : Int) => x > 10
+    
+    val predicate1 = (x : Int) => x == 10
+    val predicate2 = (x : Int) => x == 11
+    val predicate3 = (x : Int) => x == -9
+    val predicate4 = (x : Int) => x == 100
+    
+    val predicate5 = (x : Int) => x == 10000
+    val predicate6 = (x : Int) => x == -10000
+    
+    assert(!exists(set, predicate1), "Test 1")
+    assert(exists(set, predicate2), "Test 2")
+    assert(!exists(set, predicate3), "Test 3")
+    assert(exists(set, predicate4), "Test 4")
+    
+    assert(!exists(set, predicate5), "Test 5")
+    assert(!exists(set, predicate6), "Test 6")
+  } 
+ 
+  test("map test") {
+    val firstSet = (x : Int) => x > 10
+    val secondSet = (x : Int) => x < 20
+    
+    val result = intersect(firstSet, secondSet)    
+    val modificator = (x : Int) => x + 1
+        
+    assert(exists(result, x => x == 19), "Test 1 => verification")
+    assert(!exists(map(result, modificator), x => x == 19), "Test 1")
+    
+    assert(!exists(result, x => x == 10), "Test 2 => verification")
+    assert(exists(map(result, modificator), x => x == 10), "Test 2")
+    
+    assert(exists(map(result, modificator), x => x == 12), "Test 3")
+    assert(!exists(map(result, modificator), x => x == 100), "Test 4")
+    
+    assert(!exists(map(result, modificator), x => x == -1000), "Test 5")
+    assert(!exists(map(result, modificator), x => x == 1000), "Test 6")
+  }
+  
+  test("another map test") {
+    val firstSet = (x : Int) => x < 10
+    val modificator = (x : Int) => x - 1
+    val result = map(firstSet, modificator)    
+    
+    assert(exists(result, x => x == 0), "Zero test")
+    assert(exists(result, x => x == 9), "9 isn't present")
+    assert(exists(result, x => x == 10), "After moving by -1, 10 should be present")
+    assert(!exists(result, x => x == 100), "100 will be present")
+    assert(!exists(result, x => x == 1000), "Upper bound")
+    assert(!exists(result, x => x == 1001), "Upper bound + 1 won't be present in set anyway")
+  }   
 }
