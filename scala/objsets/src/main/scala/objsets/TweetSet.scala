ed242d47bd7bdec0819d731ce4d50e80d48e535d
Wojciech Gawroński <afronski@gmail.com>
2012-10-05 01:41:10 +0200

    [Scala] New idea for performance optimizations

--- scala/objsets/src/main/scala/objsets/TweetSet.scala
+++ scala/objsets/src/main/scala/objsets/TweetSet.scala
@@ -78,14 +78,15 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
   def union0(that: TweetSet, accu: TweetSet): TweetSet =
     right.union0(left,
-      left.union0(right,
-        that.union0(left,
-          that.union0(right,
-            if (!accu.contains(elem)) accu.incl(elem)
-            else accu))))
+      that.union0(left,
+        that.union0(right,
+          if (!accu.contains(elem)) accu.incl(elem)
+          else accu)))
 
-  def ascendingByRetweet0(accum: Trending): Trending =
-    this.remove(this.findMin).ascendingByRetweet0(accum + this.findMin)
+  def ascendingByRetweet0(accum: Trending): Trending = {
+    val min = this.findMin
+    this.remove(min).ascendingByRetweet0(accum + min)
+  }
 
   // The following methods are provided for you, and do not have to be changed
   // -------------------------------------------------------------------------
@@ -150,36 +151,14 @@ class NonEmptyTrending(elem: Tweet, next: Trending) extends Trending {
 
 object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
-
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
   val googleTweets: TweetSet = TweetReader.allTweets.filter(tweet => google.exists(tweet.text.contains))
-
   val appleTweets: TweetSet = TweetReader.allTweets.filter(tweet => apple.exists(tweet.text.contains))
 
-  // Q: from both sets, what is the tweet with highest #retweets?
   val trending: Trending = googleTweets.union(appleTweets).ascendingByRetweet
 }
 
 object Main extends App {
-  val set1 = new Empty
-  val set2 = set1.incl(new Tweet("a", "a body", 20))
-  val set3 = set2.incl(new Tweet("b", "b body", 20))
-  val c = new Tweet("c", "c body", 7)
-  val d = new Tweet("d", "d body", 9)
-  val set4c = set3.incl(c)
-  val set4d = set3.incl(d)
-  val set5 = set4c.incl(d)
-
-  println(set5.filter(tw => tw.user == "a"))
-  println()
-
-  println(set4c.union(set4d))
-  println()
-
-  println(set1.union(set5))
-  println()
-
-  println(set5.ascendingByRetweet)
-  println()
+  GoogleVsApple.googleTweets foreach println
 }
