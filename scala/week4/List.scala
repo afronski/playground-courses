4aaa570281e3020bceacdc686fbef4f774082560
Wojciech Gawroński <afronski@gmail.com>
2012-10-22 23:31:08 +0200

    [Scala] Apply method sample

--- scala/week4/List.scala
+++ scala/week4/List.scala
@@ -17,3 +17,9 @@ class Nil[T] extends List[T] {
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
+
+object List {
+  def apply[T](): List[T] = new Nil
+  def apply[T](x1: T): List[T] = new Cons(x1, new Nil)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+}
