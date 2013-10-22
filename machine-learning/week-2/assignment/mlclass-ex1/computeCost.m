c646a210a00b45e44c7cfed6f0900d6e706bc189
Wojciech Gawronski <afronski@gmail.com>
2013-10-22 19:46:19 +0200

    [Machine Learning] Another exercise finished (Gradient Descent).

--- machine-learning/week-2/assignment/mlclass-ex1/computeCost.m
+++ machine-learning/week-2/assignment/mlclass-ex1/computeCost.m
@@ -1,22 +1,19 @@
 function J = computeCost(X, y, theta)
-%COMPUTECOST Compute cost for linear regression
-%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
-%   parameter for linear regression to fit the data points in X and y
+  % COMPUTECOST Compute cost for linear regression
+  %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+  %   parameter for linear regression to fit the data points in X and y
 
-% Initialize some useful values
-m = length(y); % number of training examples
+  % Initialize some useful values
+  m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
+  % You need to return the following variables correctly
+  J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
+  % Instructions: Compute the cost of a particular choice of theta
+  %               You should set J to the cost.
 
+  predictions = X * theta;
+  squareErrors = (predictions - y) .^ 2;
 
-
-
-
-% =========================================================================
-
+  J = 1 / (2 * m) * sum(squareErrors);
 end
--- machine-learning/week-2/assignment/mlclass-ex1/warmUpExercise.m
+++ machine-learning/week-2/assignment/mlclass-ex1/warmUpExercise.m
@@ -1,21 +1,12 @@
 function A = warmUpExercise()
-%WARMUPEXERCISE Example function in octave
-%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+  % WARMUPEXERCISE Example function in octave
+  %   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
 
-A = [];
-% ============= YOUR CODE HERE ==============
-% Instructions: Return the 5x5 identity matrix
-%               In octave, we return values by defining which variables
-%               represent the return values (at the top of the file)
-%               and then set them accordingly.
+  A = [];
+  % Instructions: Return the 5x5 identity matrix
+  %               In octave, we return values by defining which variables
+  %               represent the return values (at the top of the file)
+  %               and then set them accordingly.
 
-
-
-A = eye(5);
-
-
-
-% ===========================================
-
-
+  A = eye(5);
 end
