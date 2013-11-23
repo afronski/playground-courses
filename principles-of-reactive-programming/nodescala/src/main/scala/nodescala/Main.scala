a2c9e56477eb5573435ec6a82313195e8266fa39
Wojciech Gawronski <afronski@gmail.com>
2013-11-23 11:52:52 +0100

    [Reactive Programming] Initial commit for 3rd week assignment (final solution will be pushed after hard deadline, at 9th of December 2013).

---
+++ principles-of-reactive-programming/nodescala/src/main/scala/nodescala/Main.scala
@@ -0,0 +1,40 @@
+package nodescala
+
+import scala.language.postfixOps
+import scala.concurrent._
+import scala.concurrent.duration._
+import ExecutionContext.Implicits.global
+import scala.async.Async.{async, await}
+
+object Main {
+
+  def main(args: Array[String]) {
+    // TO IMPLEMENT
+    // 1. instantiate the server at 8191, relative path "/test",
+    //    and have the response return headers of the request
+    val myServer = new NodeScala.Default(8191)
+    val myServerSubscription = ???
+
+    // TO IMPLEMENT
+    // 2. create a future that expects some user input `x`
+    //    and continues with a `"You entered... " + x` message
+    val userInterrupted: Future[String] = ???
+
+    // TO IMPLEMENT
+    // 3. create a future that completes after 20 seconds
+    //    and continues with a `"Server timeout!"` message
+    val timeOut: Future[String] = ???
+
+    // TO IMPLEMENT
+    // 4. create a future that completes when either 10 seconds elapse
+    //    or the user enters some text and presses ENTER
+    val terminationRequested: Future[String] = ???
+
+    // TO IMPLEMENT
+    // 5. unsubscribe from the server
+    terminationRequested onSuccess {
+      case msg => ???
+    }
+  }
+
+}
